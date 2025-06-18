74bee338ddc509a6cf3b3d2f4dde5154d215892f
itzik templeman <itzik.templeman@gmail.com>
2025-06-18 10:33:21 +0300

    d

--- src/Components/MainLayout/MainLayout.tsx
+++ src/Components/MainLayout/MainLayout.tsx
@@ -1,11 +1,26 @@
 import "./MainLayout.css";
+import {Header} from "../LayoutArea/Header/Header.tsx";
+import {Menu} from "../LayoutArea/Menu/Menu.tsx";
+import {Routing} from "../LayoutArea/Routing/Routing.tsx";
+import {Copyrights} from "../LayoutArea/Copyrights/Copyrights.tsx";
 
 export function MainLayout() {
     return (
         <div className="MainLayout">
 
-			<p>MainLayout Component</p>
+            <header>
+                <Header/>
+            </header>
 
+            <aside>
+                <Menu/>
+            </aside>
+            <main>
+                <Routing/>
+            </main>
+            <footer>
+                <Copyrights/>
+            </footer>
         </div>
     );
 }
