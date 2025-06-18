37e6b345b6a1c202f960abb4ca201e7c9c83dcf2
itzik templeman <itzik.templeman@gmail.com>
2025-06-18 10:22:51 +0300

    d

--- src/Theme/Theme.ts
+++ src/Theme/Theme.ts
@@ -1,5 +1,26 @@
 import {createTheme} from "@mui/material";
 
 export const muiTheme = createTheme(
-
-)
+    {
+        palette:
+            {
+                primary:
+                    {
+                        main: "#1e5b8c"
+                    },
+                secondary:
+                    {
+                        main: "#b2f3fd"
+                    }
+            },
+        typography:
+            {
+                allVariants:
+                    {
+                        fontFamily: "Ariel",
+                        textTransform: "none"
+                    }
+            },
+        components: {}
+    }
+);
