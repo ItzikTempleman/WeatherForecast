b25af302c2fc1cbde6c6a2ff79e701873ca1df09
itzik templeman <itzik.templeman@gmail.com>
2025-06-23 08:22:44 +0300

    d

--- src/Components/PageArea/HomeScreenArea/ForecastArea/ForecastCard/ForecastCard.tsx
+++ src/Components/PageArea/HomeScreenArea/ForecastArea/ForecastCard/ForecastCard.tsx
@@ -3,37 +3,44 @@ import "./ForecastCard.css";
 import type {ForecastDay} from "../../../../../Models/Forecast/ForecastDay.ts";
 
 
+
 type ForecastCardProps = {
     forecast: ForecastDay;
 }
 
-export function ForecastCard(props: ForecastCardProps) {
-
-    const forecast = props.forecast;
-    console.log(forecast)
+export function ForecastCard(forecastItem: ForecastCardProps) {
+    const forecast = forecastItem.forecast;
 
     if (!forecast.hour || forecast.hour.length === 0) {
-        return (
-            <div className="ForecastCard">
-                <h3>{forecast.date}</h3>
-                <><p>No hourly data available</p></>
-            </div>
-        );
+        return <p>No data available</p>;
     }
+
+    const now = new Date();
+
+
+    const futureHours = forecast.hour.filter(hourItem => {
+        if (!hourItem.time) return false;
+
+        const hourDate = new Date(hourItem.time)
+        return hourDate > now;
+        }
+    )
+
+
 
     return (
         <div className="ForecastCard">
 
             <div className="hour-list">
 
-                {forecast.hour?.map((hour, index) => (
+                {futureHours?.map((hour, index) => (
                     <div key={index} className="hour-entry">
                         <h3 className="forecast-date">{forecast.date}</h3>
                         <p>{hour.time?.split(" ")[1]}</p>
                         <p>{hour.temp_c?.toFixed(0)}°</p>
                         <p>{hour.condition?.text}</p>
                         {hour.condition?.icon && (
-                            <img src={`https:${hour.condition.icon}`} />
+                            <img src={`https:${hour.condition.icon}`}/>
                         )}
                         <p>Humidity: {hour.humidity?.toFixed(0)}%</p>
                         <p>Feels like: {hour.feelslike_c?.toFixed(0)}°</p>
